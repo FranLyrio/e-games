1b99812ab1a485838185ff5373bc1e95555a83ed
Franciele Lyrio <lyriofranciele@gmail.com>
2023-11-04 15:06:33 -0300

    refact(Text): testa componente por contextos de comportamento

--- src/components/Text/test.tsx
+++ src/components/Text/test.tsx
@@ -8,51 +8,55 @@ describe('<Text />', () => {
   it('Should render the Text component by default', () => {
     renderWithTheme(<Text>Text default</Text>)
 
-    expect(screen.getByText(/text default/i)).toHaveStyle({
-      'font-size': '1rem',
-      'font-weight': 400,
-      color: '#f5f5f5',
-      'font-family': "'Inter',sans-serif",
-    })
+    expect(screen.getByText(/text default/i)).toBeInTheDocument()
   })
 
-  it('Should render the Text component with secondary color', () => {
-    renderWithTheme(<Text color="secondary">With secondary color</Text>)
+  it('Should render the Text component by each color property', () => {
+    renderWithTheme(
+      <>
+        <Text color="main">Main color</Text>
+        <Text color="secondary">Secondary color</Text>
+      </>
+    )
 
-    expect(screen.getByText(/with secondary color/i)).toHaveStyle({
+    expect(screen.getByText(/main color/i)).toHaveStyle({
+      color: '#f5f5f5',
+    })
+
+    expect(screen.getByText(/secondary color/i)).toHaveStyle({
       color: '#f5f5f599',
     })
   })
 
-  it('Should render the Text component with body1 variant', () => {
-    renderWithTheme(<Text variant="body1">With body1 variant</Text>)
+  it('Should render the Text component by each variant', () => {
+    renderWithTheme(
+      <>
+        <Text variant="body1">With body1 variant</Text>
+        <Text variant="body2">With body2 variant</Text>
+        <Text variant="body3">With body3 variant</Text>
+        <Text variant="body4">With body4 variant</Text>
+        <Text variant="body5">With body5 variant</Text>
+      </>
+    )
 
     expect(screen.getByText(/with body1 variant/i)).toHaveStyle({
       'font-size': '0.5625rem',
     })
-  })
-
-  it('Should render the Text component with body2 variant', () => {
-    renderWithTheme(<Text variant="body2">With body2 variant</Text>)
 
     expect(screen.getByText(/with body2 variant/i)).toHaveStyle({
       'font-size': '0.6875rem',
     })
-  })
-
-  it('Should render the Text component with body3 variant', () => {
-    renderWithTheme(<Text variant="body3">With body3 variant</Text>)
 
     expect(screen.getByText(/with body3 variant/i)).toHaveStyle({
       'font-size': '0.75rem',
     })
-  })
-
-  it('Should render the Text component with body4 variant', () => {
-    renderWithTheme(<Text variant="body4">With body4 variant</Text>)
 
     expect(screen.getByText(/with body4 variant/i)).toHaveStyle({
       'font-size': '0.875rem',
+    })
+
+    expect(screen.getByText(/with body5 variant/i)).toHaveStyle({
+      'font-size': '1rem',
     })
   })
 })
